c6b875aa280c0e2891ff5a5a9132c88056b88575
Mattia2700 <mattia2700franzin+dev@gmail.com>
2023-09-24 14:35:07 +0200

    feat: add week number

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
+// get current week number
+const currentDate = new Date();
+const startDate = new Date(currentDate.getFullYear(), 0, 1);
+const days = Math.floor(
+  (currentDate.valueOf() - startDate.valueOf()) / (24 * 60 * 60 * 1000),
+);
+const weekNumber = Math.ceil(days / 7);
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center bg-[#50514F]">
     <h1 class="text-white pt-4 text-4xl font-bold">Turni pulizie</h1>
-    <h2 class="text-white py-2 text-2xl">Settimana x</h2>
+    <h2 class="text-white py-2 text-2xl">Settimana {{ weekNumber }}</h2>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
