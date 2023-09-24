e3b2163d9680494242c025d641cc26c01a40abe3
Mattia2700 <mattia2700franzin+dev@gmail.com>
2023-09-24 15:15:25 +0200

    style: add background colors

--- src/components/Shifts.vue
+++ src/components/Shifts.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const places = ["Bagno", "Cucina", "Pavimenti"]
-
+// TODO: better way to do this, like a object with place, color and text_color
+const places = ["Bagno", "Cucina", "Pavimenti"];
+const colors = ["bg-[#F25F5C]", "bg-[#FFE066]", "bg-[#247BA0]"];
+const text_colors = ["text-white", "text-black", "text-white"];
 </script>
 
 <template>
   <div class="flex flex-col h-full">
-    <h1 v-for="p in places" class="flex-1 flex items-center justify-center text-2xl">{{p}}</h1>
+    <h1
+      v-for="(p, i) in places"
+      class="flex-1 flex items-center justify-center text-2xl font-bold"
+      :class="[colors[i], text_colors[i]]"
+    >
+      {{ p }}
+    </h1>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
